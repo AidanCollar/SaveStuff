3690ab530ee3c2726cb68a9ec79b743239e00a0f
Aidan <300088221@fvtc.edu>
2024-10-09 16:12:51 -0700

    Saving data

---
+++ SaveStuff/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace SaveStuff;
+
+public partial class MainPage : ContentPage
+{
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+
+    private void Save_OnClicked(object sender, EventArgs e)
+    {
+        Preferences.Set("user", TxtUser.Text);
+    }
+
+    private void Read_OnClicked(object sender, EventArgs e)
+    {
+        TxtUser.Text=Preferences.Get("user", "");
+    }
+
+    private void Clear_OnClicked(object sender, EventArgs e)
+    {
+        TxtUser.Text = "";
+        
+    }
+}
